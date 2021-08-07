2724bc3bf0d9b9ab21b6afd8673a98172eea591d
Ren Tatsumoto <tatsu@autistici.org>
2021-08-07 21:25:15 +0300

    update packaging script

--- package.sh
+++ package.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
-zip -r JaPitch \
-	./*.py \
-	./config.* \
-	./accent_dict/ACCDB_unicode.csv \
+
+readonly ROOT_DIR=$(git rev-parse --show-toplevel)
+readonly BRANCH=$(git branch --show-current)
+readonly ZIP_NAME=ajt_pitch_accent_$BRANCH.ankiaddon
+
+cd -- "$ROOT_DIR" || exit 1
+
+export ROOT_DIR BRANCH
+
+git archive HEAD --format=zip --output "$ZIP_NAME"
+# shellcheck disable=SC2016
+git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}_${BRANCH}.zip"'
+zipmerge "$ZIP_NAME" ./*.zip
+rm -- ./*.zip
