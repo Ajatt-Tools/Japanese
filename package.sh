8f7ae1eef1e79d648e00d8bfa8e41dcbfe6708fa
Ren Tatsumoto <tatsu@autistici.org>
2021-08-14 19:32:19 +0300

    archive based on branch

--- package.sh
+++ package.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env sh
 
+readonly ADDON_NAME=ajt_pitch_accent
 readonly ROOT_DIR=$(git rev-parse --show-toplevel)
 readonly BRANCH=${1:-$(git branch --show-current)}
-readonly ZIP_NAME=ajt_pitch_accent_$BRANCH.ankiaddon
+readonly ZIP_NAME=${ADDON_NAME}_${BRANCH}.ankiaddon
 
 cd -- "$ROOT_DIR" || exit 1
 
 export ROOT_DIR BRANCH
 
-git archive HEAD --format=zip --output "$ZIP_NAME"
+git archive "$BRANCH" --format=zip --output "$ZIP_NAME"
+
 # shellcheck disable=SC2016
 git submodule foreach 'git archive main --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}_${BRANCH}.zip"'
+
 zipmerge "$ZIP_NAME" ./*.zip
 rm -- ./*.zip
