fa677a04378d4d36dc5af37169a25afc4b714b43
Ren Tatsumoto <tatsu@autistici.org>
2023-02-28 01:01:22 +0300

    convert prints to asserts

--- helpers/common_kana.py
+++ helpers/common_kana.py
@@ -24,14 +24,15 @@ def adjust_reading(raw_word: str, headword: str, headword_reading: str):
 
 
 def main():
-    print(adjust_reading('跪いた', '跪く', 'ひざまずく'))
-    print(adjust_reading('安くなかった', '安い', 'やすい'))
-    print(adjust_reading('繋りたい', '繋る', 'つながる'))
-    print(adjust_reading('言い方', '言い方', 'いいかた'))
-    print(adjust_reading('やり遂げさせられない', 'やり遂げる', 'やりとげる'))
-    print(adjust_reading('死ん', '死ぬ', 'しぬ'))
-    print(adjust_reading('たべた', 'たべる', 'たべる'))
-    print(adjust_reading('カタカナ', 'カタカナ', 'かたかな'))
+    assert (adjust_reading('跪いた', '跪く', 'ひざまずく')) == 'ひざまずいた'
+    assert (adjust_reading('安くなかった', '安い', 'やすい')) == 'やすくなかった'
+    assert (adjust_reading('繋りたい', '繋る', 'つながる')) == 'つながりたい'
+    assert (adjust_reading('言い方', '言い方', 'いいかた')) == 'いいかた'
+    assert (adjust_reading('やり遂げさせられない', 'やり遂げる', 'やりとげる')) == 'やりとげさせられない'
+    assert (adjust_reading('死ん', '死ぬ', 'しぬ')) == 'しん'
+    assert (adjust_reading('たべた', 'たべる', 'たべる')) == 'たべた'
+    assert (adjust_reading('カタカナ', 'カタカナ', 'かたかな')) == 'カタカナ'
+    print("Passed.")
 
 
 if __name__ == '__main__':
