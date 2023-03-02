29db0d9abfd67e87c50a276dc7845da230450cc6
Ren Tatsumoto <tatsu@autistici.org>
2023-03-03 02:14:51 +0300

    apply math.ceil when calculating chunk size

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,6 +1,7 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+import math
 from typing import NewType, Dict, Any, TypeVar, Sequence, Iterable
 
 import aqt
@@ -30,8 +31,17 @@ def get_notetype(note: Note) -> Dict[str, Any]:
 T = TypeVar("T")
 
 
-def split_list(input_list: Sequence[T], n_chunks: int) -> Iterable[T]:
+def split_list(input_list: Sequence[T], n_chunks: int) -> Iterable[Sequence[T]]:
     """ Splits a list into N chunks. """
-    chunk_size = len(input_list) // n_chunks
+    chunk_size = math.ceil(len(input_list) / n_chunks)
     for i in range(0, len(input_list), chunk_size):
         yield input_list[i:i + chunk_size]
+
+
+if __name__ == '__main__':
+    assert (list(split_list([1, 2, 3], n_chunks=2)) == [[1, 2], [3]])
+    assert (list(split_list([1, 2, 3, 4], n_chunks=2)) == [[1, 2], [3, 4]])
+    assert (list(split_list([1, 2, 3, 4, 5], n_chunks=2)) == [[1, 2, 3], [4, 5]])
+    assert (list(split_list([1, 2, 3, 4, 5, 6, 7], n_chunks=2)) == [[1, 2, 3, 4], [5, 6, 7]])
+    assert (list(split_list([1, 2, 3, 4, 5, 6, 7, 8], n_chunks=3)) == [[1, 2, 3], [4, 5, 6], [7, 8]])
+    print("Passed.")
