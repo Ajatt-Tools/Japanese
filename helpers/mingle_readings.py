a93e668ebc48f3f1a3b2e9dbe06d87090c6c49be
Ren Tatsumoto <tatsu@autistici.org>
2023-03-03 02:35:54 +0300

    update mingle readings
    drop support for wrapping furigana with html tags,
    as placing html tags inside <rt></rt> doesn't seem to be officially endorsed,
    and produces poor results on anki cards.
    
    annonate functions.

--- helpers/mingle_readings.py
+++ helpers/mingle_readings.py
@@ -1,7 +1,6 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import enum
 import re
 from typing import Optional, NamedTuple, List
 
@@ -17,24 +16,16 @@ class WordReading(NamedTuple):
     reading: Optional[str]
 
 
-class WordWrap(NamedTuple):
-    before: str
-    after: str
-
-
-@enum.unique
-class WordWrapMode(enum.Enum):
-    div = WordWrap('<div>', '</div>')
-    span = WordWrap('<span>', '</span>')
-    none = WordWrap('', '')
-
-
 def strip_non_jp_furigana(expr: str) -> str:
     """Non-japanese furigana is not real furigana. Strip it."""
     return re.sub(r'\[[^ぁ-ゖァ-ヺｧ-ﾝ]+]', '', expr)
 
 
-def split_furigana(text: str) -> SplitFurigana:
+def decompose_word(text: str) -> SplitFurigana:
+    """
+    Takes furigana notation, splits it into (head, reading, suffix).
+    "辛[から]い" == (head='辛', reading='から', suffix='い')
+    """
     furigana_start, furigana_end = -1, -1
     for i, c in enumerate(text):
         if c == '[':
@@ -54,13 +45,19 @@ def tie_inside_furigana(s: str) -> str:
     return re.sub(r'\[[^\[\]]+?]', fixup, s)
 
 
-def sanitize(furigana_notation: str) -> List[str]:
+def whitespace_split(furigana_notation: str) -> List[str]:
+    """
+    Splits text by whitespace, except whitespace inside furigana.
+    """
     return tie_inside_furigana(furigana_notation).split()
 
 
 def word_reading(text: str) -> WordReading:
+    """
+    Takes furigana notation, splits it into (word, reading).
+    """
     word, reading = [], []
-    for split in map(split_furigana, sanitize(text)):
+    for split in map(decompose_word, whitespace_split(text)):
         word.append(split.head + split.suffix)
         reading.append(split.reading + split.suffix)
     word, reading = ''.join(word), ''.join(reading)
@@ -71,36 +68,49 @@ def pairs(seq: List):
     yield from zip(seq, seq[1:])
 
 
-def mingle_readings(readings: List[str], *, sep: str = ', ', wrap: WordWrap = WordWrapMode.none.value) -> str:
-    """ Takes several furigana notations, packs them into one, with readings separated by sep. """
+def mingle_readings(readings: List[str], *, sep: str = ', ') -> str:
+    """
+    Takes several furigana notations, packs them into one, with readings separated by sep.
+
+    readings = ["辛[から]い", "辛[つら]い",]
+    output = " 辛[から, つら]い"
+    """
 
     assert len(readings) > 1
 
     packs = []
-    split = list(map(sanitize, readings))
+    split = list(map(whitespace_split, readings))
 
     if any(len(x) != len(y) for x, y in pairs(split)):
         return readings[0]
 
     for pack in zip(*split):
-        split = split_furigana(pack[0])
+        split = decompose_word(pack[0])
         word, readings, suffix = split.head, {split.reading: None, }, split.suffix
-        for split in map(split_furigana, pack[1:]):
+        for split in map(decompose_word, pack[1:]):
             readings[split.reading] = None
-        readings = sep.join(f"{wrap.before}{reading}{wrap.after}" for reading in readings)
+        readings = sep.join(readings)
         packs.append(f' {word}[{readings}]{suffix}' if readings != word else word)
     return ''.join(packs)
 
 
 if __name__ == '__main__':
+    assert (whitespace_split(' 有[あ]り 得[う]る') == ['有[あ]り', '得[う]る'])
     assert (strip_non_jp_furigana('悪[わる][1223]い[2]') == '悪[わる]い')
-    assert (split_furigana('故郷[こきょう]') == SplitFurigana(head='故郷', reading='こきょう', suffix=''))
-    assert (split_furigana('有[あ]り') == SplitFurigana(head='有', reading='あ', suffix='り'))
-    assert (split_furigana('ひらがな') == SplitFurigana(head='ひらがな', reading='ひらがな', suffix=''))
+    assert (decompose_word('故郷[こきょう]') == SplitFurigana(head='故郷', reading='こきょう', suffix=''))
+    assert (decompose_word('有[あ]り') == SplitFurigana(head='有', reading='あ', suffix='り'))
+    assert (decompose_word('ひらがな') == SplitFurigana(head='ひらがな', reading='ひらがな', suffix=''))
     assert (word_reading('有[あ]り 得[う]る') == WordReading(word='有り得る', reading='ありうる'))
     assert (word_reading('有る') == WordReading(word='有る', reading=None))
     assert (word_reading('お 前[まい<br>まえ<br>めえ]') == WordReading(word='お前', reading='おまい<br>まえ<br>めえ'))
-    assert (word_reading('もうお 金[かね]が 無[な]くなりました。') == WordReading(word='もうお金が無くなりました。', reading='もうおかねがなくなりました。'))
+    assert (
+            word_reading('もうお 金[かね]が 無[な]くなりました。')
+            == WordReading(word='もうお金が無くなりました。', reading='もうおかねがなくなりました。')
+    )
+    assert (
+            word_reading("妹[いもうと]は 自分[じぶん]の 我[わ]が 儘[まま]が 通[とお]らないと、すぐ 拗[す]ねる。")
+            == WordReading("妹は自分の我が儘が通らないと、すぐ拗ねる。", "いもうとはじぶんのわがままがとおらないと、すぐすねる。")
+    )
     assert (mingle_readings([' 有[あ]り 得[う]る', ' 有[あ]り 得[え]る', ' 有[あ]り 得[え]る']) == ' 有[あ]り 得[う, え]る')
     assert (mingle_readings([' 故郷[こきょう]', ' 故郷[ふるさと]']) == ' 故郷[こきょう, ふるさと]')
     assert (mingle_readings(['お 前[まえ]', 'お 前[めえ]']) == 'お 前[まえ, めえ]')
