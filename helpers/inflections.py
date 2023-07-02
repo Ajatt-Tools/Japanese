c12aa00f6a26bcb94bf4fdb42cfe86d37b9a94e5
Ren Tatsumoto <tatsu@autistici.org>
2023-07-02 14:02:41 +0300

    replace handakuten in advance to avoid incorrect comparisons

--- helpers/inflections.py
+++ helpers/inflections.py
@@ -5,10 +5,10 @@ from typing import Optional
 
 try:
     from ..mecab_controller.kana_conv import is_kana_str
-    from ..mecab_controller.unify_readings import literal_pronunciation as pr
+    from ..mecab_controller.unify_readings import replace_handakuten, literal_pronunciation as pr
 except ImportError:
     from mecab_controller.kana_conv import is_kana_str
-    from mecab_controller.unify_readings import literal_pronunciation as pr
+    from mecab_controller.unify_readings import replace_handakuten, literal_pronunciation as pr
 
 
 def longest_kana_suffix(word: str) -> Optional[str]:
@@ -22,9 +22,10 @@ def is_inflected(headword: str, reading: str) -> bool:
     Test if a reading of a verb/adjective is inflected, e.g. 臭くて, 臭かった.
     A reading is inflected if the word's kana ending isn't equal to the reading's ending.
     """
+    headword, reading = replace_handakuten(headword), replace_handakuten(reading)
     return bool(
         (kana_suffix := longest_kana_suffix(headword))
-        and pr(kana_suffix) != pr(reading)[-len(kana_suffix):]
+        and pr(kana_suffix) != pr(reading[-len(kana_suffix):])
     )
 
 
@@ -39,6 +40,7 @@ def main():
     assert is_inflected("ひらがな", "ヒラカ゚ナ") is False
     assert is_inflected("ひらがな", "ヒラカ゚ナオ") is True
     assert is_inflected("れんご", "レンコ゚") is False
+    assert is_inflected("雇う", "やとう") is False
     print("Ok.")
 
 
