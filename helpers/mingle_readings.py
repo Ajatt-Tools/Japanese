7b365ad915b7316e3a88a6dd46885f859babd0c3
Ren Tatsumoto <tatsu@autistici.org>
2023-07-26 15:58:03 +0300

    fix word-reading split: cover a corner case

--- helpers/mingle_readings.py
+++ helpers/mingle_readings.py
@@ -1,8 +1,10 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
+import io
 import re
-from typing import NamedTuple, Final
+from collections.abc import Iterable
+from typing import NamedTuple, Final, Union
 
 MULTIPLE_READING_SEP: Final[str] = '・'
 
@@ -11,6 +13,15 @@ class SplitFurigana(NamedTuple):
     head: str
     reading: str
     suffix: str
+
+
+class NoFurigana(str):
+
+    @property
+    def head(self):
+        return self
+
+    reading = head
 
 
 class WordReading(NamedTuple):
@@ -23,10 +34,10 @@ def strip_non_jp_furigana(expr: str) -> str:
     return re.sub(r'\[[^ぁ-ゖァ-ヺｧ-ﾝ]+]', '', expr)
 
 
-def decompose_word(text: str) -> SplitFurigana:
+def find_head_reading_suffix(text: str) -> Union[SplitFurigana, NoFurigana]:
     """
-    Takes furigana notation, splits it into (head, reading, suffix).
-    "辛[から]い" == (head='辛', reading='から', suffix='い')
+    Locate where furigana starts and ends, return the three parts.
+    Return text back if it doesn't contain furigana.
     """
     furigana_start, furigana_end = -1, -1
     for i, c in enumerate(text):
@@ -34,10 +45,35 @@ def decompose_word(text: str) -> SplitFurigana:
             furigana_start = i
         if c == ']':
             furigana_end = i
-    if furigana_start == furigana_end or furigana_start < 0 or furigana_end < 0:
-        return SplitFurigana(text, text, "")
-    else:
+            break
+    if 0 < furigana_start < furigana_end:
         return SplitFurigana(text[:furigana_start], text[furigana_start + 1:furigana_end], text[furigana_end + 1:])
+    else:
+        return NoFurigana(text)
+
+
+def iter_split_parts(text: str) -> Iterable[Union[SplitFurigana, NoFurigana]]:
+    while text and (part := find_head_reading_suffix(text)):
+        yield part
+        if isinstance(part, NoFurigana):
+            break
+        text = part.suffix
+
+
+def decompose_word(text: str) -> SplitFurigana:
+    """
+    Takes furigana notation, splits it into (head, reading, suffix).
+    "辛[から]い" == (head='辛', reading='から', suffix='い')
+    "南[みなみ]千[ち]秋[あき]" == (head='南千秋', reading='みなみちあき', suffix='')
+    """
+    head, reading, suffix = io.StringIO(), io.StringIO(), io.StringIO()
+    for num, part in enumerate(iter_split_parts(text)):
+        if isinstance(part, NoFurigana) and num > 0:
+            suffix.write(part)
+        else:
+            head.write(part.head)
+            reading.write(part.reading)
+    return SplitFurigana(head.getvalue(), reading.getvalue(), suffix.getvalue())
 
 
 def tie_inside_furigana(s: str) -> str:
@@ -63,14 +99,14 @@ def word_reading(text: str) -> WordReading:
         word.append(split.head + split.suffix)
         reading.append(split.reading + split.suffix)
     word, reading = ''.join(word), ''.join(reading)
-    return WordReading(word, reading) if word != reading else WordReading(text, '')
+    return WordReading(word, reading) if (reading and word != reading) else WordReading(text, '')
 
 
 def pairs(seq: list):
     yield from zip(seq, seq[1:])
 
 
-def mingle_readings(readings: list[str], *, sep: str = ', ') -> str:
+def mingle_readings(words_furigana: list[str], *, sep: str = ', ') -> str:
     """
     Takes several furigana notations, packs them into one, with readings separated by sep.
 
@@ -78,28 +114,32 @@ def mingle_readings(readings: list[str], *, sep: str = ', ') -> str:
     output = " 辛[から, つら]い"
     """
 
-    assert len(readings) > 1
+    assert len(words_furigana) > 1
 
     packs = []
-    split = list(map(whitespace_split, readings))
+    split = list(map(whitespace_split, words_furigana))
 
     if any(len(x) != len(y) for x, y in pairs(split)):
         # When notations are inconsistent, don't attempt further parsing.
-        return readings[0]
+        return words_furigana[0]
 
     for first, *rest in zip(*split):
         first = decompose_word(first)
-        readings = sep.join(dict.fromkeys(word.reading for word in (first, *map(decompose_word, rest))))
-        packs.append(f' {first.head}[{readings}]{first.suffix}' if readings != first.head else first.head)
+        words_furigana = sep.join(dict.fromkeys(word.reading for word in (first, *map(decompose_word, rest))))
+        packs.append(f' {first.head}[{words_furigana}]{first.suffix}' if words_furigana != first.head else first.head)
     return ''.join(packs)
 
 
-if __name__ == '__main__':
+def main():
     assert (whitespace_split(' 有[あ]り 得[う]る') == ['有[あ]り', '得[う]る'])
+
     assert (strip_non_jp_furigana('悪[わる][1223]い[2]') == '悪[わる]い')
+
     assert (decompose_word('故郷[こきょう]') == SplitFurigana(head='故郷', reading='こきょう', suffix=''))
     assert (decompose_word('有[あ]り') == SplitFurigana(head='有', reading='あ', suffix='り'))
     assert (decompose_word('ひらがな') == SplitFurigana(head='ひらがな', reading='ひらがな', suffix=''))
+    assert (decompose_word('南[みなみ]千[ち]秋[あき]') == SplitFurigana(head='南千秋', reading='みなみちあき', suffix=''))
+
     assert (word_reading('有[あ]り 得[う]る') == WordReading(word='有り得る', reading='ありうる'))
     assert (word_reading('有る') == WordReading(word='有る', reading=''))
     assert (word_reading('お 前[まい<br>まえ<br>めえ]') == WordReading(word='お前', reading='おまい<br>まえ<br>めえ'))
@@ -111,8 +151,13 @@ if __name__ == '__main__':
             word_reading("妹[いもうと]は 自分[じぶん]の 我[わ]が 儘[まま]が 通[とお]らないと、すぐ 拗[す]ねる。")
             == WordReading("妹は自分の我が儘が通らないと、すぐ拗ねる。", "いもうとはじぶんのわがままがとおらないと、すぐすねる。")
     )
+
     assert (mingle_readings([' 有[あ]り 得[う]る', ' 有[あ]り 得[え]る', ' 有[あ]り 得[え]る']) == ' 有[あ]り 得[う, え]る')
     assert (mingle_readings([' 故郷[こきょう]', ' 故郷[ふるさと]']) == ' 故郷[こきょう, ふるさと]')
     assert (mingle_readings(['お 前[まえ]', 'お 前[めえ]']) == 'お 前[まえ, めえ]')
     assert (mingle_readings([' 言[い]い 分[ぶん]', ' 言い分[いーぶん]']) == ' 言[い]い 分[ぶん]')
     print("Passed.")
+
+
+if __name__ == '__main__':
+    main()
