5df2bd9340ebb8a52f388f0f0ecad5a212845747
Ren Tatsumoto <tatsu@autistici.org>
2023-11-01 20:13:06 +0300

    rename function

--- helpers/common_kana.py
+++ helpers/common_kana.py
@@ -7,7 +7,7 @@ except ImportError:
     from mecab_controller.kana_conv import to_katakana as _
 
 
-def adjust_reading(raw_word: str, headword: str, headword_reading: str):
+def adjust_to_inflection(raw_word: str, headword: str, headword_reading: str) -> str:
     """
     Adjusts the word's reading to match its conjugated form.
     E.g., if raw_word is 食べた, should output たべた.
@@ -28,14 +28,14 @@ def adjust_reading(raw_word: str, headword: str, headword_reading: str):
 
 
 def main():
-    assert (adjust_reading('跪いた', '跪く', 'ひざまずく')) == 'ひざまずいた'
-    assert (adjust_reading('安くなかった', '安い', 'やすい')) == 'やすくなかった'
-    assert (adjust_reading('繋りたい', '繋る', 'つながる')) == 'つながりたい'
-    assert (adjust_reading('言い方', '言い方', 'いいかた')) == 'いいかた'
-    assert (adjust_reading('やり遂げさせられない', 'やり遂げる', 'やりとげる')) == 'やりとげさせられない'
-    assert (adjust_reading('死ん', '死ぬ', 'しぬ')) == 'しん'
-    assert (adjust_reading('たべた', 'たべる', 'たべる')) == 'たべた'
-    assert (adjust_reading('カタカナ', 'カタカナ', 'かたかな')) == 'カタカナ'
+    assert (adjust_to_inflection('跪いた', '跪く', 'ひざまずく')) == 'ひざまずいた'
+    assert (adjust_to_inflection('安くなかった', '安い', 'やすい')) == 'やすくなかった'
+    assert (adjust_to_inflection('繋りたい', '繋る', 'つながる')) == 'つながりたい'
+    assert (adjust_to_inflection('言い方', '言い方', 'いいかた')) == 'いいかた'
+    assert (adjust_to_inflection('やり遂げさせられない', 'やり遂げる', 'やりとげる')) == 'やりとげさせられない'
+    assert (adjust_to_inflection('死ん', '死ぬ', 'しぬ')) == 'しん'
+    assert (adjust_to_inflection('たべた', 'たべる', 'たべる')) == 'たべた'
+    assert (adjust_to_inflection('カタカナ', 'カタカナ', 'かたかな')) == 'カタカナ'
     print("Passed.")
 
 
