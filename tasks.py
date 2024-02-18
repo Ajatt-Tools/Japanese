0531badaf91f54ffcabd5d10cb025dd7e2da3447
Ren Tatsumoto <tatsu@autistici.org>
2024-02-18 09:45:32 +0300

    use taskman to download audio

--- tasks.py
+++ tasks.py
@@ -81,11 +81,8 @@ class AddAudio(DoTask, task_type=ProfileAudio):
             ignore_inflections=cfg.audio_settings.ignore_inflections,
             stop_if_one_source_has_results=cfg.audio_settings.stop_if_one_source_has_results,
         )[:cfg.audio_settings.maximum_results]
-
-        self._aud_src_mgr.download_and_save_tags(
-            search_results,
-            run_in_background=(self._caller.cfg.audio_download_report is True),
-        )
+        # Download and save tags has to run on main as it will launch a new QueryOp.
+        mw.taskman.run_on_main(functools.partial(self._aud_src_mgr.download_and_save_tags, search_results,))
         return format_audio_tags(search_results)
 
 
