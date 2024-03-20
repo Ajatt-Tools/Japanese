6c4666ad4f54b19e8a7441700a250f97b1477118
Ren Tatsumoto <tatsu@autistici.org>
2024-03-20 17:51:55 +0300

    update package sh

--- package.sh
+++ package.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
-
-readonly NC='\033[0m'
-readonly RED='\033[0;31m'
-readonly GREEN='\033[0;32m'
-
-rm -v -- ./*.ankiaddon 2>/dev/null || true
-
-for exe in git zip zipmerge; do
-	if ! command -v "$exe" >/dev/null; then
-		echo -e "${RED}Missing dependency:${NC} $exe"
-		exit 1
-	fi
-done
-
-readonly ADDON_NAME=ajt_japanese
-readonly ROOT_DIR=$(git rev-parse --show-toplevel)
-readonly BRANCH=${1:-$(git branch --show-current)}
-readonly ZIP_NAME=${ADDON_NAME}_${BRANCH}.ankiaddon
-
-cd -- "$ROOT_DIR" || exit 1
-export ROOT_DIR BRANCH
-git archive "$BRANCH" --format=zip --output "$ZIP_NAME"
-# shellcheck disable=SC2016
-git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}_${BRANCH}.zip"'
-zipmerge "$ZIP_NAME" ./*.zip
-rm -- ./*.zip
-echo -e "${GREEN}Done.${NC}"
+./ajt_common/package.sh \
+	--package "Japanese" \
+	--name "AJT Japanese" \
+	"$@"
