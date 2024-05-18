13f72edc4abad307d1b7e11cea5c994958762f72
Ren Tatsumoto <tatsu@autistici.org>
2024-05-18 21:44:52 +0300

    use points

--- japanese/pitch_accents/svg_graphs.py
+++ japanese/pitch_accents/svg_graphs.py
@@ -6,6 +6,7 @@
 import dataclasses
 import enum
 import math
+import typing
 from collections.abc import Iterable
 from enum import Enum
 from math import sqrt
@@ -74,10 +75,12 @@ def make_group(elements: Iterable[str], classname: str) -> str:
     return f'<g class="{classname}">{"".join(elements)}</g>'
 
 
-@dataclasses.dataclass
-class Point:
+class Point(typing.NamedTuple):
     x: float
     y: float
+
+    def shift_by(self, *, x: float = 0, y: float = 0):
+        return Point(x=self.x + x, y=self.y + y)
 
 
 class Line:
@@ -90,12 +93,12 @@ class Line:
         self.start = None
         self.end = None
 
-    def start_at(self, x1: float, y1: float) -> "Line":
-        self.start = Point(x1, y1)
+    def start_at(self, pos: Point) -> "Line":
+        self.start = pos
         return self
 
-    def end_at(self, x2: float, y2: float) -> "Line":
-        self.end = Point(x2, y2)
+    def end_at(self, pos: Point) -> "Line":
+        self.end = pos
         return self
 
     def is_completed(self) -> bool:
@@ -122,18 +125,14 @@ class Line:
         offset_x = r * cos
 
         if self.start.y == self.end.y:
-            self.start.x += r
-            self.end.x -= r
+            self.start = self.start.shift_by(x=r)
+            self.end = self.end.shift_by(x=-r)
         elif self.start.y > self.end.y:
-            self.start.x += offset_x
-            self.start.y -= offset_y
-            self.end.x -= offset_x
-            self.end.y += offset_y
+            self.start = self.start.shift_by(x=offset_x, y=-offset_y)
+            self.end = self.end.shift_by(x=-offset_x, y=offset_y)
         elif self.start.y < self.end.y:
-            self.start.x += offset_x
-            self.start.y += offset_y
-            self.end.x -= offset_x
-            self.end.y -= offset_y
+            self.start = self.start.shift_by(x=offset_x, y=offset_y)
+            self.end = self.end.shift_by(x=-offset_x, y=-offset_y)
 
         return self
 
@@ -150,23 +149,23 @@ class Path:
     def last(self) -> Line:
         return self._lines[-1]
 
-    def start_at(self, x: float, y: float) -> "Path":
-        self._lines.append(Line(self._opts).start_at(x, y))
+    def start_at(self, pos: Point) -> "Path":
+        self._lines.append(Line(self._opts).start_at(pos))
         return self
 
-    def go_to(self, x: int, y: int) -> "Path":
-        self.last.end_at(x, y)
+    def go_to(self, pos: Point) -> "Path":
+        self.last.end_at(pos)
         return self
 
-    def push(self, x: int, y: int) -> "Path":
+    def push(self, pos: Point) -> "Path":
         if len(self._lines) == 0:
-            self.start_at(x, y)
+            self.start_at(pos)
         elif self.last.is_completed():
             assert self.last.end
-            self.start_at(self.last.end.x, self.last.end.y)
-            self.go_to(x, y)
+            self.start_at(self.last.end)
+            self.go_to(pos)
         else:
-            self.go_to(x, y)
+            self.go_to(pos)
         return self
 
     def draw(self, trailing: bool = False) -> str:
@@ -182,7 +181,7 @@ class SvgPitchGraphMaker:
     def __init__(self, options: SvgPitchGraphOptions):
         self._opts = options
 
-    def make_circle(self, x_pos: int, y_pos: int, trailing: bool = False) -> str:
+    def make_circle(self, pos: Point, trailing: bool = False) -> str:
         """
         Create a circle that is positioned where two lines touch.
         """
@@ -190,24 +189,24 @@ class SvgPitchGraphMaker:
         stroke = "gray" if trailing else "black"
         return (
             f'<circle fill="{fill}" stroke="{stroke}" stroke-width="{self._opts.stroke_width:.2f}" '
-            f'cx="{x_pos:.3f}" cy="{y_pos:.3f}" r="{self._opts.circle_radius:.2f}" />'
+            f'cx="{pos.x:.3f}" cy="{pos.y:.3f}" r="{self._opts.circle_radius:.2f}" />'
         )
 
-    def make_devoiced_circle(self, mora: Mora, x_pos: int, y_pos: int) -> str:
+    def make_devoiced_circle(self, mora: Mora, pos: Point) -> str:
         """
         Make a circle around the mora text to show that it's devoiced.
         """
         if len(mora.txt) == 1:
-            x_pos = int(x_pos + self._opts.font_size / 2 + self._opts.text_dx)
-            y_pos = int(y_pos + self._opts.text_dx + math.ceil(self._opts.stroke_width))
+            x_pos = int(pos.x + self._opts.font_size / 2 + self._opts.text_dx)
+            y_pos = int(pos.y + self._opts.text_dx + math.ceil(self._opts.stroke_width))
             return (
                 f'<circle class="devoiced" fill="none" stroke="black" cx="{x_pos:.0f}" cy="{y_pos:.0f}" '
                 f'stroke-width="{self._opts.devoiced_circle_width:.2f}" r="{self._opts.devoiced_circle_radius:.2f}" '
                 f'stroke-dasharray="{self._opts.devoiced_stroke_disarray}" />'
             )
         else:
-            x_pos = int(x_pos - self._opts.font_size - self._opts.devoiced_rectangle_padding)
-            y_pos = int(y_pos - self._opts.font_size - math.floor(self._opts.stroke_width))
+            x_pos = int(pos.x - self._opts.font_size - self._opts.devoiced_rectangle_padding)
+            y_pos = int(pos.y - self._opts.font_size - math.floor(self._opts.stroke_width))
             width = self._opts.font_size * 2 + self._opts.devoiced_rectangle_padding * 2
             height = self._opts.devoiced_circle_radius * 2
             return (
@@ -217,7 +216,7 @@ class SvgPitchGraphMaker:
                 f'stroke-dasharray="{self._opts.devoiced_stroke_disarray}" />'
             )
 
-    def make_text(self, mora: Mora, x: int, y: int, dx: int) -> str:
+    def make_text(self, mora: Mora, pos: Point, dx: int) -> str:
         """
         Create a text element with the mora inside.
         """
@@ -229,11 +228,11 @@ class SvgPitchGraphMaker:
         )
         return (
             f'<text{append_classname(mora.flags)} fill="black" font-size="{self._opts.font_size}px" '
-            f'x="{x:.0f}" y="{y:.0f}" dx="{dx:.0f}">{mora.txt}{quark}</text>'
+            f'x="{pos.x:.0f}" y="{pos.y:.0f}" dx="{dx:.0f}">{mora.txt}{quark}</text>'
         )
 
     def calc_svg_width(self, moras: list[Mora], pitch_type: PitchType) -> int:
-        count = len(moras) + (1 if pitch_type == PitchType.heiban or len(moras) == 1 else 0)
+        count = len(moras) + (1 if pitch_type == PitchType.heiban or len(moras) == 1 else 0)  # todo
         return count * self._opts.x_step + self._opts.graph_horizontal_padding * 2
 
     def make_svg(self, contents: str, *, width: int, height: int, visible_height: int) -> str:
@@ -245,8 +244,8 @@ class SvgPitchGraphMaker:
     def make_trailing_line(self, start: Point, end: Point) -> str:
         opts = self._opts
         # 1-mora heiban words start low, so the last mora is still low.
-        trail_line = Path(opts).start_at(start.x, start.y).go_to(end.x, end.y).draw(trailing=True)
-        trail_circle = self.make_circle(end.x, end.y, trailing=True)
+        trail_line = Path(opts).start_at(start).go_to(end).draw(trailing=True)
+        trail_circle = self.make_circle(end, trailing=True)
         return make_group([trail_line, trail_circle], "trail")
 
     def make_graph(self, entry: FormattedEntry) -> str:
@@ -254,36 +253,36 @@ class SvgPitchGraphMaker:
         moras = entry_to_moras(entry)
         pitch_type = pitch_type_from_pitch_num(moras, entry.pitch_number)
 
-        x_pos = y_pos = height_high = opts.size_unit
-        x_pos += opts.graph_horizontal_padding
+        height_high = opts.size_unit
         height_low = height_high + opts.graph_height
         height_kana = height_low + opts.x_step
+        pos = Point(opts.size_unit + opts.graph_horizontal_padding, opts.size_unit)
 
         word_circles: list[str] = []
         text_moras: list[str] = []
         path = Path(opts)
 
         for idx, mora in enumerate(moras):
-            y_pos = height_high if mora.level == PitchLevel.high else height_low
-            word_circles.append(self.make_circle(x_pos, y_pos))
-            path.push(x_pos, y_pos)
+            pos = Point(pos.x, height_high if mora.level == PitchLevel.high else height_low)
+            word_circles.append(self.make_circle(pos))
+            path.push(pos)
 
             if MoraFlag.devoiced in mora.flags:
                 # circle around text
-                text_moras.append(self.make_devoiced_circle(mora, x_pos, height_kana))
+                text_moras.append(self.make_devoiced_circle(mora, Point(pos.x, height_kana)))
 
-            text_moras.append(self.make_text(mora, x_pos, height_kana, dx=int(opts.text_dx) * len(mora.txt)))
+            text_moras.append(self.make_text(mora, Point(pos.x, height_kana), dx=int(opts.text_dx) * len(mora.txt)))
 
-            x_pos += opts.x_step
+            pos = Point(pos.x + opts.x_step, pos.y)
 
         content: list[str] = []
 
-        if pitch_type == PitchType.heiban or len(moras) == 1:
-            assert height_high == y_pos or len(moras) == 1, f"can't proceed: {entry}"
+        if pitch_type == PitchType.heiban or len(moras) == 1:  # todo
+            assert height_high == pos.y or len(moras) == 1, f"can't proceed: {entry}"
             content.append(
                 self.make_trailing_line(
-                    start=Point(x_pos - opts.x_step, y_pos),
-                    end=Point(x_pos, height_low if pitch_type == PitchType.atamadaka else height_high),
+                    start=Point(pos.x - opts.x_step, pos.y),
+                    end=Point(pos.x, height_low if pitch_type == PitchType.atamadaka else height_high),
                 )
             )
 
