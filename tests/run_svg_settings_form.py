dbe2c4fde457cf6a0b121f21e8c24623d6769fce
Ren Tatsumoto <tatsu@autistici.org>
2024-06-04 08:23:48 +0300

    use widget SvgSettingsWidget

--- tests/run_svg_settings_form.py
+++ tests/run_svg_settings_form.py
@@ -3,23 +3,8 @@
 from aqt.qt import *
 
 from japanese.config_view import SvgPitchGraphOptionsConfigView
-from japanese.pitch_accents.common import FormattedEntry
-from japanese.pitch_accents.svg_graphs import SvgPitchGraphMaker
-from japanese.widgets.settings_form import SvgSettingsForm
+from japanese.widgets.svg_settings import SvgSettingsWidget
 from tests.run_context_menu_form import NoAnkiConfigView
-
-SHOW_ENTRIES = (
-    FormattedEntry(
-        "オンガクカ",
-        "<low_rise>オ</low_rise><high>ンカ<nasal>&#176;</nasal><devoiced>ク</devoiced>カ</high>",
-        "0",
-    ),
-    FormattedEntry(
-        "スウカゲツ",
-        "<low_rise>ス</low_rise><high_drop>ウカ</high_drop><low>ゲツ</low>",
-        "3",
-    ),
-)
 
 
 class MockWindow(QDialog):
@@ -27,40 +12,15 @@ class MockWindow(QDialog):
         super().__init__()
         self._config = NoAnkiConfigView()
         self._svg_config = SvgPitchGraphOptionsConfigView(NoAnkiConfigView())
-        self._form = SvgSettingsForm(self._svg_config)
-        self._view = QWebEngineView()
-        self._maker = SvgPitchGraphMaker(options=self._svg_config)
+        self._settings_widget = SvgSettingsWidget(self._svg_config)
         self.initUI()
-        self.update_view()
-        qconnect(self._form.opts_changed, self.update_view)
-
-    def update_view(self) -> None:
-        self._svg_config.update(self._form.as_dict())
-        self._view.setHtml(
-            """
-            <style>
-            body {
-                display: flex;
-                flex-flow: row wrap;
-                gap: 5px;
-                align-content: flex-start;
-            }
-            svg {
-                display: block;
-                border: 1px dotted black;
-            }
-            </style>
-            """
-            + "".join(self._maker.make_graph(entry) for entry in SHOW_ENTRIES)
-        )
 
     def initUI(self):
         self.setWindowTitle("Settings Form")
         self.setMinimumSize(300, 300)
 
         layout = QVBoxLayout()
-        layout.addWidget(self._form)
-        layout.addWidget(self._view)
+        layout.addWidget(self._settings_widget)
         button = QPushButton("Save")
         layout.addWidget(button)
         qconnect(button.clicked, self.accept)
@@ -68,7 +28,7 @@ class MockWindow(QDialog):
         self.setLayout(layout)
 
     def accept(self):
-        print(f"{self._form.as_dict()=}")
+        print(f"{self._settings_widget.as_dict()=}")
         return super().accept()
 
 
