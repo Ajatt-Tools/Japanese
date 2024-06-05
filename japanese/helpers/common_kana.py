eab6ad1ef404a837b76833c1eb7ebd6a147f90a3
Ren Tatsumoto <tatsu@autistici.org>
2024-06-05 12:32:32 +0300

    move asserts

--- japanese/helpers/common_kana.py
+++ japanese/helpers/common_kana.py
@@ -1,12 +1,8 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-try:
-    from ..mecab_controller.kana_conv import is_kana_str
-    from ..mecab_controller.kana_conv import to_katakana as _
-except ImportError:
-    from mecab_controller.kana_conv import is_kana_str
-    from mecab_controller.kana_conv import to_katakana as _
+from ..mecab_controller.kana_conv import is_kana_str
+from ..mecab_controller.kana_conv import to_katakana as _
 
 
 def adjust_to_inflection(raw_word: str, headword: str, headword_reading: str) -> str:
@@ -35,32 +31,3 @@ def adjust_to_inflection(raw_word: str, headword: str, headword_reading: str) ->
     if _(stem_reading) == _(headword_reading):
         return headword_reading
     return stem_reading + inflected_reading
-
-
-# Debug
-##########################################################################
-
-
-def main():
-    assert adjust_to_inflection("食べた", "食べる", "たべる") == "たべた"
-    assert adjust_to_inflection("跪いた", "跪く", "ひざまずく") == "ひざまずいた"
-    assert adjust_to_inflection("跪かなかった", "跪く", "ひざまずく") == "ひざまずかなかった"
-    assert adjust_to_inflection("安くなかった", "安い", "やすい") == "やすくなかった"
-    assert adjust_to_inflection("繋りたい", "繋る", "つながる") == "つながりたい"
-    assert adjust_to_inflection("言い方", "言い方", "いいかた") == "いいかた"
-    assert adjust_to_inflection("やり遂げさせられない", "やり遂げる", "やりとげる") == "やりとげさせられない"
-    assert adjust_to_inflection("死ん", "死ぬ", "しぬ") == "しん"
-    assert adjust_to_inflection("たべた", "たべる", "たべる") == "たべた"
-    assert adjust_to_inflection("カタカナ", "カタカナ", "かたかな") == "カタカナ"
-    assert adjust_to_inflection("相合い傘", "相合い傘", "あいあいがさ") == "あいあいがさ"
-    assert adjust_to_inflection("いた目", "板目", "いため") == "いため"
-    assert adjust_to_inflection("軽そう", "軽装", "けいそー") == "けいそー"
-    assert adjust_to_inflection("唸りました", "唸る", "うなる") == "うなりました"
-    assert adjust_to_inflection("可愛くない", "可愛い", "かわいい") == "かわいくない"
-    assert adjust_to_inflection("かわいくない", "可愛い", "かわいい") == "かわいくない"
-
-    print("Passed.")
-
-
-if __name__ == "__main__":
-    main()
---
+++ tests/test_common_kana.py
@@ -0,0 +1,22 @@
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+from japanese.helpers.common_kana import adjust_to_inflection
+
+
+def test_adjust_to_inflection() -> None:
+    assert adjust_to_inflection("食べた", "食べる", "たべる") == "たべた"
+    assert adjust_to_inflection("跪いた", "跪く", "ひざまずく") == "ひざまずいた"
+    assert adjust_to_inflection("跪かなかった", "跪く", "ひざまずく") == "ひざまずかなかった"
+    assert adjust_to_inflection("安くなかった", "安い", "やすい") == "やすくなかった"
+    assert adjust_to_inflection("繋りたい", "繋る", "つながる") == "つながりたい"
+    assert adjust_to_inflection("言い方", "言い方", "いいかた") == "いいかた"
+    assert adjust_to_inflection("やり遂げさせられない", "やり遂げる", "やりとげる") == "やりとげさせられない"
+    assert adjust_to_inflection("死ん", "死ぬ", "しぬ") == "しん"
+    assert adjust_to_inflection("たべた", "たべる", "たべる") == "たべた"
+    assert adjust_to_inflection("カタカナ", "カタカナ", "かたかな") == "カタカナ"
+    assert adjust_to_inflection("相合い傘", "相合い傘", "あいあいがさ") == "あいあいがさ"
+    assert adjust_to_inflection("いた目", "板目", "いため") == "いため"
+    assert adjust_to_inflection("軽そう", "軽装", "けいそー") == "けいそー"
+    assert adjust_to_inflection("唸りました", "唸る", "うなる") == "うなりました"
+    assert adjust_to_inflection("可愛くない", "可愛い", "かわいい") == "かわいくない"
+    assert adjust_to_inflection("かわいくない", "可愛い", "かわいい") == "かわいくない"
