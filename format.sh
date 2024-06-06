4e8491f90fc138a05536d17469db48c4ff7ba61c
Ren Tatsumoto <tatsu@autistici.org>
2024-06-07 02:43:15 +0300

    update format

--- format.sh
+++ format.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./japanese/ajt_common/format.sh
+echo "Formatting $PWD"
+
+readonly ROOT_DIR=$(git rev-parse --show-toplevel)
+readarray -t FILES <<<"$(find "$ROOT_DIR" -iname '*.py')"
+readonly -a FILES
+
+pyupgrade --py39-plus "${FILES[@]}"
+isort "${FILES[@]}"
+black "${FILES[@]}"
