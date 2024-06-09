2dab384f6ce9d087b3b1c2d321ace72e732ea0b5
Ren Tatsumoto <tatsu@autistici.org>
2024-06-09 23:57:04 +0300

    call code from ajt common

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "Formatting $PWD"
-
 readonly ROOT_DIR=$(git rev-parse --show-toplevel)
-readarray -t FILES <<<"$(find "$ROOT_DIR" -iname '*.py')"
-readonly -a FILES
 
-pyupgrade --py39-plus "${FILES[@]}"
-isort "${FILES[@]}"
-black "${FILES[@]}"
+"$ROOT_DIR/japanese/ajt_common/format.sh"
