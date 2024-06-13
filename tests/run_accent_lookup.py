9df244eca23d3b65df629dbb51cf0eb3f437712d
Ren Tatsumoto <tatsu@autistici.org>
2024-06-13 03:28:56 +0300

    add run accent lookup

---
+++ tests/run_accent_lookup.py
@@ -0,0 +1,17 @@
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+from japanese.mecab_controller import to_katakana
+from japanese.pitch_accents.acc_dict_mgr import read_formatted_accents
+
+
+def main() -> None:
+    acc_dict = read_formatted_accents()
+    for item in acc_dict[to_katakana("経緯")]:
+        print(item)
+    for item in acc_dict[to_katakana("国境")]:
+        print(item)
+
+
+if __name__ == "__main__":
+    main()
