ae42a1fcf63bab6280848a772be176554a6d4b77
Ren Tatsumoto <tatsu@autistici.org>
2024-06-14 22:30:39 +0300

    refactor user accents

--- japanese/pitch_accents/user_accents.py
+++ japanese/pitch_accents/user_accents.py
@@ -3,26 +3,29 @@
 
 import collections
 import os
-from collections.abc import Iterable, MutableMapping, Sequence
+from collections.abc import Iterable, MutableMapping
 from typing import NamedTuple, Union
 
 from ..helpers.file_ops import touch, user_files_dir
 from ..mecab_controller.kana_conv import kana_to_moras, to_katakana
-from .common import AccentDict, FormattedEntry
+from .common import AccentDict, FormattedEntry, OrderedSet, repack_accent_dict
 from .consts import NO_ACCENT
 from .format_accents import format_entry
+
+USER_DATA_CSV_PATH = os.path.join(user_files_dir(), "user_data.tsv")
 
 
 def search_pitch_accent_numbers(accents: str) -> Iterable[Union[str, int]]:
     return ((int(pos) if pos != NO_ACCENT else NO_ACCENT) for pos in accents.split(","))
 
 
-class AccentEntry(NamedTuple):
+class TSVAccentEntry(NamedTuple):
     """Represents a parsed entry in the user TSV file."""
 
     headword: str
     moras: tuple[str, ...]
     accents: tuple[Union[str, int], ...]
+    katakana_reading: str
 
     def has_accent(self):
         return self.accents != NO_ACCENT
@@ -30,17 +33,19 @@ class AccentEntry(NamedTuple):
     @classmethod
     def from_csv_line(cls, line: str):
         headword, reading, accents = line.split("\t")
+        reading = to_katakana(reading or headword)
         return cls(
             headword=headword,
-            moras=tuple(kana_to_moras(to_katakana(reading or headword))),
+            moras=tuple(kana_to_moras(reading)),
             accents=tuple(dict.fromkeys(search_pitch_accent_numbers(accents))),
+            katakana_reading=reading,
         )
 
 
-def create_formatted(entry: AccentEntry) -> MutableMapping[FormattedEntry, None]:
-    return dict.fromkeys(
+def create_formatted(entry: TSVAccentEntry) -> OrderedSet[FormattedEntry]:
+    return OrderedSet.fromkeys(
         FormattedEntry(
-            katakana_reading="".join(entry.moras),
+            katakana_reading=entry.katakana_reading,
             html_notation=format_entry(entry.moras, pitch_num),
             pitch_number=str(pitch_num),
         )
@@ -49,42 +54,25 @@ def create_formatted(entry: AccentEntry) -> MutableMapping[FormattedEntry, None]
 
 
 class UserAccentData:
-    source_csv_path = os.path.join(user_files_dir(), "user_data.tsv")
+    _source_csv_path: str = USER_DATA_CSV_PATH
 
     def __init__(self):
-        self.self_check()
+        self._self_check()
 
-    def self_check(self):
-        if not os.path.isfile(self.source_csv_path):
-            touch(self.source_csv_path)
-            print(f"Created file: {self.source_csv_path}")
+    def _self_check(self):
+        if not os.path.isfile(self._source_csv_path):
+            touch(self._source_csv_path)
+            print(f"Created file: {self._source_csv_path}")
 
-    def read_entries(self) -> Iterable[AccentEntry]:
-        with open(self.source_csv_path, encoding="utf-8") as f:
+    def _read_entries(self) -> Iterable[TSVAccentEntry]:
+        with open(self._source_csv_path, newline="", encoding="utf-8") as f:
             for line in f:
                 if line := line.strip():
-                    yield AccentEntry.from_csv_line(line)
+                    yield TSVAccentEntry.from_csv_line(line)
 
     def create_formatted(self) -> AccentDict:
         """Build the derived pitch accents file from the original pitch accents file and save it as *.csv"""
-        temp_dict = collections.defaultdict(dict)
-        for entry in self.read_entries():
-            temp_dict[entry.headword].update(create_formatted(entry))
-        for key in temp_dict:
-            temp_dict[key] = tuple(temp_dict[key])
-        return AccentDict(temp_dict)
-
-
-# Debug
-##########################################################################
-
-
-def main():
-    data = UserAccentData()
-    formatted = data.create_formatted()
-    for key, value in formatted.items():
-        print(f"{key=}; {value=}")
-
-
-if __name__ == "__main__":
-    main()
+        temp_dict: dict[str, OrderedSet[FormattedEntry]] = collections.defaultdict(OrderedSet)
+        for entry in self._read_entries():
+            temp_dict[to_katakana(entry.headword)].update(create_formatted(entry))
+        return repack_accent_dict(temp_dict)
