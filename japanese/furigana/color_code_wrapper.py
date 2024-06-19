2fdecb2cf037fe266ab8d9fc148d6d52db97f511
Ren Tatsumoto <tatsu@autistici.org>
2024-06-19 08:58:36 +0300

    allow outputting multiple formats at the same time

--- japanese/furigana/color_code_wrapper.py
+++ japanese/furigana/color_code_wrapper.py
@@ -54,10 +54,11 @@ class ColorCodeWrapper(io.StringIO):
         self._output_format = output_format
         self._set_coloring()
         if self._coloring_enabled:
-            self.start_wrap()
+            self._start_wrap()
 
     def _set_coloring(self) -> None:
-        if self._output_format == ColorCodePitchFormat.none:
+        assert isinstance(self._output_format, ColorCodePitchFormat)
+        if self._output_format == ColorCodePitchFormat(0):
             # color code feature is disabled completely. skip.
             self._coloring_enabled = False
         elif should_skip_coloring(self._token):
@@ -68,39 +69,39 @@ class ColorCodeWrapper(io.StringIO):
 
     def getvalue(self) -> str:
         if self._coloring_enabled:
-            self.end_wrap()
+            self._end_wrap()
         return super().getvalue()
 
-    def start_wrap(self) -> None:
-        assert self._output_format != ColorCodePitchFormat.none
+    def _start_wrap(self) -> None:
+        assert self._output_format != ColorCodePitchFormat(0)
         self.write(f'<span class="ajt__word_info"')
-        self._write_attributes()
-        self.write(">")
-
-    def end_wrap(self) -> None:
-        assert self._output_format != ColorCodePitchFormat.none
-        self.write("</span>")
-
-    def _write_attributes(self):
-        assert self._output_format != ColorCodePitchFormat.none
-        if self._output_format == ColorCodePitchFormat.attributes:
+        if ColorCodePitchFormat.attributes in self._output_format:
             self.write(f' part_of_speech="{self._token.part_of_speech.name}"')
             self.write(f' pitch="{self._token.describe_pitches()}"')
-        elif html_color := get_main_pitch_color(self._token):
+        if html_color := get_main_pitch_color(self._token):
             self._write_inline_color(html_color)
+        self.write(">")
+
+    def _end_wrap(self) -> None:
+        assert self._output_format != ColorCodePitchFormat(0)
+        self.write("</span>")
 
     def _write_inline_color(self, html_color: str) -> None:
         """
         add inline styles for people who don't configure their css templates
         """
-        assert self._output_format != ColorCodePitchFormat.none
+        assert self._output_format != ColorCodePitchFormat(0)
         # apply inline styles only when the pitch accent is not ambiguous
-        if self._output_format == ColorCodePitchFormat.color:
-            self.write(f' style="color: {html_color}"')
-        elif self._output_format == ColorCodePitchFormat.underline:
+        if self._output_format & ~ColorCodePitchFormat.attributes == ColorCodePitchFormat(0):
+            return
+        self.write(f' style="')
+        if ColorCodePitchFormat.color in self._output_format:
+            self.write(f'color: {html_color};')
+        if ColorCodePitchFormat.color | ColorCodePitchFormat.underline in self._output_format:
+            self.write(" ")
+        if ColorCodePitchFormat.underline in self._output_format:
             self.write(
-                f' style="text-decoration: underline; text-decoration-color: {html_color};'
-                f' text-decoration-thickness: 2px; text-underline-offset: 3px;"'
+                f'text-decoration: underline; text-decoration-color: {html_color};'
+                f' text-decoration-thickness: 2px; text-underline-offset: 3px;'
             )
-        else:
-            raise ValueError(f"can't handle {self._output_format}")
+        self.write('"')
