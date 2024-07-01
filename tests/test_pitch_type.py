2ae676f3663ead14d9db059b6d6b8a0eba140bf3
Ren Tatsumoto <tatsu@autistici.org>
2024-07-01 23:46:30 +0300

    update tests
    add tests for count_moras()

--- tests/test_pitch_type.py
+++ tests/test_pitch_type.py
@@ -1,6 +1,22 @@
 # Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-from japanese.pitch_accents.basic_types import PitchType, pitch_type_from_pitch_num
+from japanese.mecab_controller.kana_conv import HIRAGANA, KATAKANA
+from japanese.pitch_accents.basic_types import (
+    PitchType,
+    count_moras,
+    pitch_type_from_pitch_num,
+)
+
+
+def test_count_moras() -> None:
+    assert count_moras("あいうえお") == 5
+    assert count_moras("カキクケコ") == 5
+    assert count_moras("にゃ") == 1
+    assert count_moras("あ") == 1
+    assert count_moras("ー") == 1
+    assert count_moras("っ") == 1
+    assert count_moras("とうきょう") == 4
+    assert count_moras("きゃきゅきょ") == 3
 
 
 def test_pitch_type_from_pitch_num() -> None:
