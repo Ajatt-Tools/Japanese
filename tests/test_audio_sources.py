3eb8dc4288cd506c544711801001ebdb94f09cbe
Ren Tatsumoto <tatsu@autistici.org>
2024-07-02 13:28:22 +0300

    add test

--- tests/test_audio_sources.py
+++ tests/test_audio_sources.py
@@ -9,15 +9,22 @@ def some_media_dir() -> str:
     return "https://raw.githubusercontent.com/Ajatt-Tools/nhk_2016_pronunciations_index/main/media"
 
 
-def get_test_source(db: Sqlite3Buddy) -> AudioSource:
-    return AudioSource(enabled=True, name="Test", url="https://example.com", db=db)
-
-
-def test_source_join(no_anki_config: NoAnkiConfigView, monkeypatch) -> None:
+def test_source_join_remote(no_anki_config: NoAnkiConfigView, monkeypatch) -> None:
     monkeypatch.setattr(AudioSource, "is_cached", lambda _self: True)
     monkeypatch.setattr(AudioSource, "is_local", False)
     monkeypatch.setattr(Sqlite3Buddy, "get_media_dir_abs", lambda _self, _name: some_media_dir())
     with sqlite3_buddy() as db:
-        source = get_test_source(db)
+        source = AudioSource(enabled=True, name="Test 1", url="https://example.com", db=db)
         assert source.media_dir == some_media_dir()
         assert source.join(source.media_dir, "filename.ogg") == f"{some_media_dir()}/filename.ogg"
+
+
+def test_source_join_local(no_anki_config: NoAnkiConfigView, monkeypatch) -> None:
+    monkeypatch.setattr(AudioSource, "is_cached", lambda _self: True)
+    monkeypatch.setattr(AudioSource, "is_local", True)
+    monkeypatch.setattr(Sqlite3Buddy, "get_media_dir_abs", lambda _self, _name: None)
+    monkeypatch.setattr(Sqlite3Buddy, "get_media_dir_rel", lambda _self, _name: "media")
+    with sqlite3_buddy() as db:
+        source = AudioSource(enabled=True, name="Test 2", url="/path/to/index.json", db=db)
+        assert source.media_dir == "/path/to/media"
+        assert source.join(source.media_dir, "filename.ogg") == f"/path/to/media/filename.ogg"
