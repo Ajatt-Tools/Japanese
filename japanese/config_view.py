2c854ce10eee6122ef3098b4dcf52b413f25bcb5
Ren Tatsumoto <tatsu@autistici.org>
2024-07-10 07:33:41 +0300

    remove unused import

--- japanese/config_view.py
+++ japanese/config_view.py
@@ -1,7 +1,6 @@
 # Copyright: Ren Tatsumoto <tatsu at autistici.org>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import dataclasses
 import enum
 import functools
 import re
