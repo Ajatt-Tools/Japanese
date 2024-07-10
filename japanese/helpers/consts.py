18a79949cc695fb6960075a54997d6c72311f9c7
Ren Tatsumoto <tatsu@autistici.org>
2024-07-10 07:31:13 +0300

    place consts in a separate file

---
+++ japanese/helpers/consts.py
@@ -0,0 +1,9 @@
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
+
+from ..ajt_common.consts import ADDON_SERIES
+
+LONG_VOWEL_MARK: str = "ー"
+ADDON_NAME: str = f"{ADDON_SERIES} Japanese"
+THIS_ADDON_MODULE: str = __name__.split(".")[0]
+CFG_WORD_SEP = ","
