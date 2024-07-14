5e9cf835156dc0bd3c4cbed0993f1dde916a8452
Ren Tatsumoto <tatsu@autistici.org>
2024-07-14 12:16:53 +0300

    allow styling handakuten separately from the nasal character
    this change was requested by @scylithe:matrix.org

--- japanese/pitch_accents/styles.py
+++ japanese/pitch_accents/styles.py
@@ -30,105 +30,109 @@ class XmlTags:
     # high accent, going down ‾‾‾\
     high_drop_start = "<high_drop>"
     high_drop_end = "</high_drop>"  # &#42780; (ꜜ)
-    # NHK data only:
-    nasal_start = "<nasal>"  # Red color &#176; (°)
-    nasal_end = "</nasal>"
+    # Devoiced, e.g. シュ (NHK data only)
     devoiced_start = "<devoiced>"
     devoiced_end = "</devoiced>"
+    # Nasal, e.g. コ° (NHK data only)
+    nasal_start = "<nasal>"
+    nasal_end = "</nasal>"
+    # Nasal mark (NHK data only). It should be added after the nasal kana character (e.g. '°' in コ°).
+    handakuten_start = "<handakuten>"  # &#176; (°), colored red.
+    handakuten_end = "</handakuten>"
 
 
 PITCH_COLOR_PLACEHOLDER = "[PITCH_COLOR]"
+STYLE_MAP: dict[PitchPatternStyle, dict[str, str]] = dict()
 
-STYLE_MAP = {
-    PitchPatternStyle.javdejong: {
-        # Style used in the original Japanese Pitch Accent Anki add-on.
-        # Low accents aren't marked, high accents are marked with an overline.
-        # low
-        XmlTags.low_start: "",
-        XmlTags.low_end: "",
-        # low, rise at the end
-        XmlTags.low_rise_start: "",
-        XmlTags.low_rise_end: "",
-        # high
-        XmlTags.high_start: '<span style="text-decoration:overline;">',
-        XmlTags.high_end: "</span>",
-        # high, drop at the end
-        XmlTags.high_drop_start: '<span style="text-decoration:overline;">',
-        XmlTags.high_drop_end: "</span>&#42780;",  # down arrow at the end
-        # nasal, e.g. カ゚
-        XmlTags.nasal_start: '<span style="color: red;">',
-        XmlTags.nasal_end: "</span>",
-        # devoiced
-        XmlTags.devoiced_start: '<span style="color: royalblue;">',
-        XmlTags.devoiced_end: "</span>",
-    },
-    PitchPatternStyle.u_biq_color_coded: {
-        # Style used on the u-biq website, https://accent.u-biq.org/
-        # low
-        XmlTags.low_start: f'<span style="box-shadow: inset 0px -2px 0 0px {PITCH_COLOR_PLACEHOLDER};">',
-        XmlTags.low_end: "</span>",
-        # low, rise at the end
-        XmlTags.low_rise_start: f'<span style="box-shadow: inset -2px -2px 0 0 {PITCH_COLOR_PLACEHOLDER};">',
-        XmlTags.low_rise_end: "</span>",
-        # high
-        XmlTags.high_start: f'<span style="box-shadow: inset 0px 2px 0 0px {PITCH_COLOR_PLACEHOLDER};">',
-        XmlTags.high_end: "</span>",
-        # high, drop at the end
-        XmlTags.high_drop_start: f'<span style="box-shadow: inset -2px 2px 0 0px {PITCH_COLOR_PLACEHOLDER};">',
-        XmlTags.high_drop_end: "</span>",
-        # nasal, e.g. カ゚
-        XmlTags.nasal_start: '<span style="color: red;">',
-        XmlTags.nasal_end: "</span>",
-        # devoiced
-        XmlTags.devoiced_start: '<span style="color: royalblue;">',
-        XmlTags.devoiced_end: "</span>",
-    },
-    PitchPatternStyle.none: {
-        # Use class names.
-        # The user can configure their own styles in the Styling section of the card type.
-        # low
-        XmlTags.low_start: '<span class="low">',
-        XmlTags.low_end: "</span>",
-        # low, rise at the end
-        XmlTags.low_rise_start: '<span class="low_rise">',
-        XmlTags.low_rise_end: "</span>",
-        # high
-        XmlTags.high_start: '<span class="high">',
-        XmlTags.high_end: "</span>",
-        # high, drop at the end
-        XmlTags.high_drop_start: '<span class="high_drop">',
-        XmlTags.high_drop_end: "</span>",
-        # nasal, e.g. カ゚
-        XmlTags.nasal_start: '<span class="nasal">',
-        XmlTags.nasal_end: "</span>",
-        # devoiced
-        XmlTags.devoiced_start: '<span class="devoiced">',
-        XmlTags.devoiced_end: "</span>",
-    },
-    PitchPatternStyle.kanjium: {
-        # Style which is part of the kanjium project https://github.com/mifunetoshiro/kanjium
-        # low
-        XmlTags.low_start: "",
-        XmlTags.low_end: "",
-        # low, rise at the end
-        XmlTags.low_rise_start: "",
-        XmlTags.low_rise_end: "",
-        # high
-        XmlTags.high_start: '<span style="display:inline-block;position:relative;"><span style="display:inline;">',
-        XmlTags.high_end: '</span><span style="border-color:currentColor;display:block;user-select:none;pointer-events:none;position:absolute;top:0.1em;left:0;right:0;height:0;border-top-width:0.1em;border-top-style:solid;"></span></span>',
-        # high, drop at the end
-        XmlTags.high_drop_start: '<span style="display:inline-block;position:relative;padding-right:0.1em;margin-right:0.1em;"><span style="display:inline;">',
-        XmlTags.high_drop_end: '</span><span style="border-color:currentColor;display:block;user-select:none;pointer-events:none;position:absolute;top:0.1em;left:0;right:0;height:0;border-top-width:0.1em;border-top-style:solid;right:-0.1em;height:0.4em;border-right-width:0.1em;border-right-style:solid;"></span></span>',
-        # nasal, e.g. カ゚
-        XmlTags.nasal_start: '<span style="color: red;">',
-        XmlTags.nasal_end: "</span>",
-        # devoiced
-        XmlTags.devoiced_start: '<span style="color: royalblue;">',
-        XmlTags.devoiced_end: "</span>",
-    },
+# javdejong: style used in the original Japanese Pitch Accent Anki add-on.
+# Low accents aren't marked, high accents are marked with an overline.
+STYLE_MAP[PitchPatternStyle.javdejong] = {
+    # low
+    XmlTags.low_start: "",
+    XmlTags.low_end: "",
+    # low, rise at the end
+    XmlTags.low_rise_start: "",
+    XmlTags.low_rise_end: "",
+    # high
+    XmlTags.high_start: '<span style="text-decoration:overline;">',
+    XmlTags.high_end: "</span>",
+    # high, drop at the end
+    XmlTags.high_drop_start: '<span style="text-decoration:overline;">',
+    XmlTags.high_drop_end: "</span>&#42780;",  # down arrow at the end
+    # nasal, e.g. カ゚
+    XmlTags.nasal_start: "",
+    XmlTags.nasal_end: "",
+    # handakuten (°)
+    XmlTags.handakuten_start: '<span style="color: red;">',
+    XmlTags.handakuten_end: "</span>",
+    # devoiced
+    XmlTags.devoiced_start: '<span style="color: royalblue;">',
+    XmlTags.devoiced_end: "</span>",
 }
+
+# u-biq: style used on the u-biq website, https://accent.u-biq.org/
+# This version has color-coded lines.
+STYLE_MAP[PitchPatternStyle.u_biq_color_coded] = {
+    **STYLE_MAP[PitchPatternStyle.javdejong],
+    # low
+    XmlTags.low_start: f'<span style="box-shadow: inset 0px -2px 0 0px {PITCH_COLOR_PLACEHOLDER};">',
+    XmlTags.low_end: "</span>",
+    # low, rise at the end
+    XmlTags.low_rise_start: f'<span style="box-shadow: inset -2px -2px 0 0 {PITCH_COLOR_PLACEHOLDER};">',
+    XmlTags.low_rise_end: "</span>",
+    # high
+    XmlTags.high_start: f'<span style="box-shadow: inset 0px 2px 0 0px {PITCH_COLOR_PLACEHOLDER};">',
+    XmlTags.high_end: "</span>",
+    # high, drop at the end
+    XmlTags.high_drop_start: f'<span style="box-shadow: inset -2px 2px 0 0px {PITCH_COLOR_PLACEHOLDER};">',
+    XmlTags.high_drop_end: "</span>",
+}
+
+# u-biq: style used on the u-biq website, https://accent.u-biq.org/
+# This version has orange lines (like the original).
 STYLE_MAP[PitchPatternStyle.u_biq] = {
-    # plain u-biq has a fixed color
-    k: v.replace(PITCH_COLOR_PLACEHOLDER, "#FF6633")
-    for k, v in STYLE_MAP[PitchPatternStyle.u_biq_color_coded].items()
+    k: v.replace(PITCH_COLOR_PLACEHOLDER, "#FF6633") for k, v in STYLE_MAP[PitchPatternStyle.u_biq_color_coded].items()
+}
+
+# kanjium: style which is part of the kanjium project https://github.com/mifunetoshiro/kanjium
+STYLE_MAP[PitchPatternStyle.kanjium] = {
+    **STYLE_MAP[PitchPatternStyle.javdejong],
+    # low
+    XmlTags.low_start: "",
+    XmlTags.low_end: "",
+    # low, rise at the end
+    XmlTags.low_rise_start: "",
+    XmlTags.low_rise_end: "",
+    # high
+    XmlTags.high_start: '<span style="display:inline-block;position:relative;"><span style="display:inline;">',
+    XmlTags.high_end: '</span><span style="border-color:currentColor;display:block;user-select:none;pointer-events:none;position:absolute;top:0.1em;left:0;right:0;height:0;border-top-width:0.1em;border-top-style:solid;"></span></span>',
+    # high, drop at the end
+    XmlTags.high_drop_start: '<span style="display:inline-block;position:relative;padding-right:0.1em;margin-right:0.1em;"><span style="display:inline;">',
+    XmlTags.high_drop_end: '</span><span style="border-color:currentColor;display:block;user-select:none;pointer-events:none;position:absolute;top:0.1em;left:0;right:0;height:0;border-top-width:0.1em;border-top-style:solid;right:-0.1em;height:0.4em;border-right-width:0.1em;border-right-style:solid;"></span></span>',
+}
+
+# none: use class names.
+# The user can configure their own styles in the Styling section of the card template.
+STYLE_MAP[PitchPatternStyle.none] = {
+    # low
+    XmlTags.low_start: '<span class="low">',
+    XmlTags.low_end: "</span>",
+    # low, rise at the end
+    XmlTags.low_rise_start: '<span class="low_rise">',
+    XmlTags.low_rise_end: "</span>",
+    # high
+    XmlTags.high_start: '<span class="high">',
+    XmlTags.high_end: "</span>",
+    # high, drop at the end
+    XmlTags.high_drop_start: '<span class="high_drop">',
+    XmlTags.high_drop_end: "</span>",
+    # nasal, e.g. カ゚
+    XmlTags.nasal_start: '<span class="nasal">',
+    XmlTags.nasal_end: "</span>",
+    # handakuten (°)
+    XmlTags.handakuten_start: '<span class="handakuten">',
+    XmlTags.handakuten_end: "</span>",
+    # devoiced
+    XmlTags.devoiced_start: '<span class="devoiced">',
+    XmlTags.devoiced_end: "</span>",
 }
