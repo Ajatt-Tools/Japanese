2245220f8fbb802beeb2b5b8dfc927b225369f85
Ren Tatsumoto <tatsu@autistici.org>
2024-07-20 19:24:54 +0300

    update file ops to use pathlib

--- japanese/helpers/file_ops.py
+++ japanese/helpers/file_ops.py
@@ -3,8 +3,10 @@
 
 import functools
 import os
+import pathlib
 import subprocess
 from collections.abc import Iterable
+from typing import Union
 
 from anki.utils import no_bundled_libs
 
@@ -13,42 +15,45 @@ from ..ajt_common.utils import find_executable
 THIS_ADDON_MODULE = __name__.split(".")[0]
 
 
-def walk_parents(current_dir: str) -> Iterable[str]:
-    while not os.path.samefile(parent_dir := os.path.dirname(current_dir), current_dir):
+def walk_parents(dir_or_file_path: Union[str, pathlib.Path]) -> Iterable[pathlib.Path]:
+    current_dir = pathlib.Path(dir_or_file_path)
+    if current_dir.is_dir():
+        yield current_dir
+    while not current_dir.samefile(parent_dir := current_dir.parent):
         yield parent_dir
         current_dir = parent_dir
 
 
-def touch(path) -> None:
+def touch(path: Union[str, pathlib.Path]) -> None:
     with open(path, "a"):
         os.utime(path, None)
 
 
-def rm_file(path) -> None:
+def rm_file(path: Union[str, pathlib.Path]) -> None:
     try:
         os.unlink(path)
     except FileNotFoundError:
         pass
 
 
-def find_file_in_parents(file_name: str) -> str:
+def find_file_in_parents(file_name: str) -> pathlib.Path:
     """Used when testing/debugging."""
     for parent_dir in walk_parents(__file__):
-        if os.path.isfile(path := os.path.join(parent_dir, file_name)):
+        if (path := parent_dir.joinpath(file_name)).is_file():
             return path
     raise RuntimeError(f"couldn't find file '{file_name}'")
 
 
-def find_config_json() -> str:
+def find_config_json() -> pathlib.Path:
     """Used when testing/debugging."""
     return find_file_in_parents("config.json")
 
 
 @functools.cache
-def user_files_dir() -> str:
+def user_files_dir() -> pathlib.Path:
     """Return path to the user files directory."""
     for parent_dir in walk_parents(__file__):
-        if os.path.isdir(dir_path := os.path.join(parent_dir, "user_files")):
+        if (dir_path := parent_dir.joinpath("user_files")).is_dir():
             return dir_path
     raise RuntimeError("couldn't find user_files directory")
 
@@ -77,4 +82,4 @@ def open_file(path: str) -> None:
 
 
 def file_exists(file_path: str) -> bool:
-    return file_path and os.path.isfile(file_path) and os.stat(file_path).st_size > 0
+    return bool(file_path and os.path.isfile(file_path) and os.stat(file_path).st_size > 0)
