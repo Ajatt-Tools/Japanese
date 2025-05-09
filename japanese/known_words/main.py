4a40858437906ad36504d09e059f2ca0487aaf87
Ren Tatsumoto <tatsu@autistici.org>
2025-05-09 20:10:31 +0300

    add copyright

--- japanese/known_words/main.py
+++ japanese/known_words/main.py
@@ -1,3 +1,5 @@
+# Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 import csv
 import datetime
 import logging
